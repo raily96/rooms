abf6e45ab155514b7d5bf6c1d856518a71d48ce4
raily96 <fogliailudovico@gmail.com>
2025-03-12 23:46:52 +0100

    Add files via upload

---
+++ HabboHotel/Rooms/Handshake.cs
@@ -0,0 +1,51 @@
+﻿
+using Butterfly.Core;
+using System;
+namespace Butterfly.Messages
+{
+    partial class GameClientMessageHandler
+    {
+        internal void SendSessionParams()
+        {
+            Response.Init(257);
+            Response.AppendInt32(9);
+            Response.AppendInt32(0);
+            Response.AppendInt32(0);
+            Response.AppendInt32(1);
+            Response.AppendInt32(1);
+            Response.AppendInt32(3);
+            Response.AppendInt32(0);
+            Response.AppendInt32(2);
+            Response.AppendInt32(1);
+            Response.AppendInt32(4);
+            Response.AppendInt32(1);
+            Response.AppendInt32(5);
+            Response.AppendStringWithBreak("dd-MM-yyyy");
+            Response.AppendInt32(7);
+            Response.AppendBoolean(false);
+            Response.AppendInt32(8);
+            Response.AppendStringWithBreak("/client");
+            Response.AppendInt32(9);
+            Response.AppendBoolean(false);
+
+            SendResponse();
+        }
+
+        internal void SSOLogin()
+        {
+            Session.tryLogin(Request.PopFixedString());
+        }
+
+        //internal void RegisterHandshake()
+        //{
+        //    RequestHandlers.Add(206, new RequestHandler(SendSessionParams));
+        //    RequestHandlers.Add(415, new RequestHandler(SSOLogin));
+        //}
+
+        //internal void UnRegisterHandshake()
+        //{
+        //    RequestHandlers.Remove(206);
+        //    RequestHandlers.Remove(415);
+        //}
+    }
+}
---
+++ HabboHotel/Rooms/RoomHelper.cs
@@ -0,0 +1,22 @@
+﻿using Butterfly.Collections;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butterfly.HabboHotel.Rooms
+{
+    public static class RoomHelper
+    {
+        public static ClonedTable GetThreadSafeTable()
+        {
+            // Implementazione del metodo per ottenere una tabella thread-safe
+            // Puoi personalizzare questa implementazione in base alle tue esigenze specifiche
+            return new ClonedTable(
+                // Assumendo che tu abbia una Hashtable da clonare
+                new Hashtable()); // Sostituisci con la tua hashtable da clonare
+        }
+    }
+}
